f34d6de4cda8328fe805fb15e1eced8cc32fd43a
carlosjunod <carlos@junod.cl>
2020-08-16 00:15:50 -0400

    add click/hover

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas, useThree, extend } from "react-three-fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./App.css";
 
+extend({ OrbitControls });
+
 const Cube = (props) => {
+  const [isBig, setIsBig] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const size = isBig ? 2 : 1;
+  const color = isHovered ? "salmon" : "tomato";
+
   return (
-    <mesh {...props}>
-      <boxBufferGeometry attach='geometry' args={[2, 2, 1]} />
-      <meshStandardMaterial attach='material' color='tomato' />
+    <mesh
+      {...props}
+      onClick={() => setIsBig(!isBig)}
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+    >
+      <boxBufferGeometry attach='geometry' args={[size, size, size]} />
+      <meshStandardMaterial attach='material' color={color} />
     </mesh>
   );
 };
 // args = [width, height, depth]
-
-extend({ OrbitControls });
 
 const Scene = () => {
   const {
