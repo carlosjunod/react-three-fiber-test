0c07cb594d174df11ee7c9af5dfe14b4c6e34112
carlosjunod <carlos@junod.cl>
2020-08-15 23:58:59 -0400

    adding orbit control

--- src/App.js
+++ src/App.js
@@ -1,64 +1,44 @@
-import React, { useState } from "react";
-import { Canvas } from "react-three-fiber";
+import React from "react";
+import { Canvas, useThree, extend } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./App.css";
 
-const Cube = ({ width, height, depth }) => {
+const Cube = (props) => {
   return (
-    <mesh position={[0, 0, 0]} rotation={[10, 20, 0]}>
-      <boxBufferGeometry attach='geometry' args={[width, height, depth]} />
+    <mesh {...props}>
+      <boxBufferGeometry attach='geometry' args={[2, 2, 1]} />
       <meshStandardMaterial attach='material' color='tomato' />
     </mesh>
   );
 };
 // args = [width, height, depth]
 
-const Scene = (props) => {
+extend({ OrbitControls });
+
+const Scene = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
   return (
     <>
       <ambientLight />
       <pointLight intensity={0.3} position={[-1, -2, 4]} />
       <pointLight intensity={0.1} position={[2, -10, 10]} />
-      <Cube {...props.cube} />
+      <Cube position={[0, 0, 0]} rotation={[10, 20, 0]} />
+      <Cube position={[-2, 1, 0]} rotation={[10, 30, 10]} />
+
+      <orbitControls args={[camera, domElement]} />
     </>
   );
 };
 
 function App() {
-  const [width, setWidth] = useState(1);
-  const [height, setHeight] = useState(1);
-  const [depth, setDepth] = useState(1);
-
   return (
     <>
-      <input
-        onChange={(e) => setWidth(e.target.value)}
-        type='range'
-        id='width'
-        name='width'
-        min='1'
-        max='10'
-        step={0.1}
-      />
-      <input
-        onChange={(e) => setHeight(e.target.value)}
-        type='range'
-        id='height'
-        name='height'
-        min='1'
-        max='10'
-        step={0.1}
-      />
-      <input
-        onChange={(e) => setDepth(e.target.value)}
-        type='range'
-        id='depth'
-        name='depth'
-        min='1'
-        max='10'
-        step={0.1}
-      />
       <Canvas>
-        <Scene cube={{ width, height, depth }} />
+        <Scene />
       </Canvas>
     </>
   );
