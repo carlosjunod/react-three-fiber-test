1f0a09aeb101776efa17527d4e31b8773b6d05e0
carlosjunod <carlos@junod.cl>
2020-08-18 21:36:31 -0400

    Updates

---
+++ precache-manifest.776eec58a03da549af8d4938b4b46351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5ce8282725052b513e04310814c89b",
+    "url": "/3dtest/index.html"
+  },
+  {
+    "revision": "dae829152816f0214ce0",
+    "url": "/3dtest/static/css/main.d821e160.chunk.css"
+  },
+  {
+    "revision": "f157034d996890ef324c",
+    "url": "/3dtest/static/js/2.68a8d0b4.chunk.js"
+  },
+  {
+    "revision": "9fe6fa133e08bd5dc10e3d0c6927893f",
+    "url": "/3dtest/static/js/2.68a8d0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae829152816f0214ce0",
+    "url": "/3dtest/static/js/main.fc7ac0a9.chunk.js"
+  },
+  {
+    "revision": "9a74fbe54407c834bb02",
+    "url": "/3dtest/static/js/runtime-main.2cff5264.js"
+  }
+]);
